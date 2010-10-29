6993b3bf00a949649ea0d91e6b2f951a9eac1e44
Albert Astals Cid <tsdgeos@terra.es>
2010-10-29 18:52:32 +0000

    fix sytnax
    svn path=/trunk/kdereview/nepomukshell/; revision=1191153

--- Messages.sh
+++ Messages.sh
@@ -1,3 +1,3 @@
 #! /usr/bin/env bash
-$EXTRACTRC `find . -name "*.ui" -o -name "*.kcfg` >> rc.cpp
+$EXTRACTRC `find . -name "*.ui" -o -name "*.kcfg"` >> rc.cpp
 $XGETTEXT `find . -name "*.cpp"` -o $podir/nepomukshell.pot
